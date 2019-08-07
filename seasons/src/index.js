9da41230285758aad3808aa6c026af5ea2631abe
oz izhak <oz@web-pick.com>
2019-08-07 17:54:38 +0300

    seasson app

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -12,7 +12,7 @@ class App extends React.Component {
 
     render() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
+            (position) => {},
             (err) => console.log(err)
         );
 
