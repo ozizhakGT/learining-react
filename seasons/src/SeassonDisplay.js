433df16e85ed4e99226380469836592a7a5eafb6
oz izhak <oz@web-pick.com>
2019-08-07 17:54:22 +0300

    seasson app

---
+++ seasons/src/SeassonDisplay.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SeassonDisplay = () => {
+    return <div>Seasson Display</div>
+};
+
+
+export default SeassonDisplay;
---
+++ seasons/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeassonDisplay from './SeassonDisplay';
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {lat: null};
+    }
+
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+
+
+        return <div > Latitude: {this.state.lat}</div>
+    }
+}
+
+ReactDOM.render( <App /> , document.getElementById('root'));
