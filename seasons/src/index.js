71bed4b31fe3594e081a8d1c565811dd1c7adbdf
oz izhak <ozizhak5540@gmail.com>
2019-08-08 00:23:13 +0300

    finish component class style

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -7,17 +7,25 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {lat: null};
+        this.state = {lat: null, errorMassege: ''};
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat: position.coords.latitude});
+            },
+            (err) => {
+                this.setState({errorMassege: err.message})
+            }
+        );
     }
 
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {},
-            (err) => console.log(err)
-        );
-
-
-        return <div > Latitude: {this.state.lat}</div>
+        if(this.state.errorMassege && !this.state.lat) {
+            return <div>Error: {this.state.errorMassege}</div>
+        }
+        if(!this.state.errorMassege && this.state.lat) {
+            return <div>Latittude: {this.state.lat}</div>
+        }
+        return <div>Loading...</div>
     }
 }
 
