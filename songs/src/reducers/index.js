7e317166bb6211db91058dde40f579ef5bcf41e4
oz izhak <ozizhak5540@gmail.com>
2019-08-11 03:05:11 +0300

    last commit

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,17 @@
+const songsReducer = () => {
+    return [
+        {title: 'If I Was Ready', duration: '4:05'},
+        {title: 'Smell Like Teen Spirit', duration: '5:15'},
+        {title: 'smoke On The Water', duration: '3:42'},
+        {title: 'I Want It That Way', duration: '3:00'}
+    ];
+};
+
+
+const SelectedSongReducer = (selectedSong=null, action) => {
+    if(action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectedSong;
+};
